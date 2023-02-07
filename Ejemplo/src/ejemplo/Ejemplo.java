9045df7164c5ac1fb9c56184a9c9b581340c1ae9
Bryan Paez <bryanpaez.125@gmail.com>
2023-02-06 21:55:29 -0600

    [ADD] Se imprimio Hola mundo

--- Ejemplo/src/ejemplo/Ejemplo.java
+++ Ejemplo/src/ejemplo/Ejemplo.java
@@ -4,17 +4,12 @@
  */
 package ejemplo;
 
-/**
- *
- * @author gerar
- */
 public class Ejemplo {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        for (int i = 0; i < 10; i++) {
+                    System.out.println("imprime plox");
+
+        }
     }
     
 }
